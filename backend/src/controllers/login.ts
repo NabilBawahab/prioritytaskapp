7400ab0f207b932e9ce950aeeaf092650e482f71
NabilBawahab <nabilbawahab@gmail.com>
2025-07-10 15:26:17 +0700

    flow login selesai

--- backend/src/controllers/login.ts
+++ backend/src/controllers/login.ts
@@ -1,6 +1,7 @@
 import type { Request, Response } from "express";
 import { prisma } from "../utils/prisma";
 import bcrypt from "bcrypt";
+import jwt from "jsonwebtoken";
 
 const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
 
@@ -43,5 +44,34 @@ export const loginAction = async (
       });
       return;
     }
+
+    const isPasswordValid: boolean = await bcrypt.compare(
+      password,
+      user.password,
+    );
+
+    if (!isPasswordValid) {
+      res.status(401);
+      res.json({
+        status: res.statusCode,
+        message: "Invalid credential",
+      });
+      return;
+    }
+
+    const token = jwt.sign(
+      { userId: user.id, email: user.email },
+      process.env.JWT_SECRET!,
+      { expiresIn: "1h" },
+    );
+
+    res.status(200);
+    res.json({
+      status: res.statusCode,
+      message: "Login successful",
+      data: {
+        token,
+      },
+    });
   } catch (error) {}
 };
