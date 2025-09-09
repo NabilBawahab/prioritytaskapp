f1de895fe289b556706205e5ea52edb17e91eaf2
NabilBawahab <nabilbawahab@gmail.com>
2025-09-09 14:33:03 +0700

    tes buat AI

--- backend/src/controllers/amyai.ts
+++ backend/src/controllers/amyai.ts
@@ -1,6 +1,18 @@
 import type { Request, Response } from "express";
-export const amyAi = (req: Request, res: Response) => {
+import { openai } from "../utils/openai";
+export const amyAi = async (req: Request, res: Response) => {
   const { message } = req.body;
 
   console.log(message);
+  const completion = await openai.chat.completions.create({
+    model: process.env.OPENROUTER_AI_MODEL as string,
+    messages: [
+      {
+        role: "user",
+        content: message,
+      },
+    ],
+  });
+
+  console.log(completion.choices[0].message.content);
 };
